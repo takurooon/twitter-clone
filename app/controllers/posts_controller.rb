d474e130757bf9a753cef17dc9a0dd3b4a279acf
児玉拓郎 <takurokodama@takuroooons-MacBook-Pro.local>
2018-07-16 15:52:01 +0900

    Add posts_controller protect_from_forgery

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   protect_from_forgery :except => [:create]
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end

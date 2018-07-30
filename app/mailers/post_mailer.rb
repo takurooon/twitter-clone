6eae21931bd8740384a67b5d915f03109b7739a0
児玉拓郎 <takurokodama@takuroooons-MacBook-Pro.local>
2018-07-30 20:02:13 +0900

    Add sendgrid_username_password

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,7 +2,7 @@ class PostMailer < ApplicationMailer
 
   def post_mail(post)
     @post = post
-    mail to: "自分のメールアドレス", subject: "お問い合わせの確認メール"
+    mail to: "mailaddress0726@icloud.com", subject: "お問い合わせの確認メール"
   end
 
 end
